247f2b8fa7e2b6fdef6436fe51cb2b45b37dd7db
TrishonParrilla <trishonp1@gmail.com>
2026-04-29 20:11:16 -0600

    Add pages and templates for Home and About views, update URL routing

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.http import HttpResponse
 
 # Create your views here.
+#Class based views
+class HomePageView(TemplateView):
+    template_name = "pages/home.html"
+
+class AboutPageView(TemplateView):
+    template_name = "pages/about.html"
+
+#Function based Views
+def contact_me(request):
+    return HttpResponse("Hello world from a Function Based View")
