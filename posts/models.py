15547b924fae0a313a39915c39b08240c08bd1a3
TrishonParrilla <trishonp1@gmail.com>
2026-05-02 15:07:08 -0600

    added Post model and initial migration; included fields for title, subtitle, body, created_on, and author

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=128)#string
+    subtitle = models.CharField(max_length=256)#string
+    body = models.TextField() #string
+    created_on = models.DateField(auto_now_add=True)#date/datetime
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete = models.CASCADE
+    ) #user Model/foreign key to user model
