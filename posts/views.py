2569ff1abdabd4c9cc201d276cf1b6957a9b1957
TrishonParrilla <trishonp1@gmail.com>
2026-05-02 16:15:12 -0600

    Implement PostListView and create list template; update navbar for post navigation

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import (
+    ListView
+)
 
+from .models import Post
+class PostListView(ListView):
+    #template_name is the attribute to render the html
+    template_name = "posts/list.html"
+    #model attribute lets django know from which model(table) we want to retrieve data
+    model = Post 
+    #context object name allows us to change the variable name on how we call it inside of templates
+    context_object_name = "posts"
 # Create your views here.
