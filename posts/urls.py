5cc884a4264daa6850f996c77914c678c5de2777
TrishonParrilla <trishonp1@gmail.com>
2026-05-04 19:58:03 -0600

    Add PostDetailView and update templates for post detail and list views

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import (
-    PostListView
+    PostListView,
+    PostDetailView
 )
 
 
 urlpatterns = [
     path("", PostListView.as_view(), name="post_list" ),
+    path("<int:pk>/", PostDetailView.as_view(), name="post_detail")
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import (
-    ListView
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
 )
 
 from .models import Post
-class PostListView(ListView):
+class PostListView(ListView): #Get request that returns lists of posts from DB
     #template_name is the attribute to render the html
     template_name = "posts/list.html"
     #model attribute lets django know from which model(table) we want to retrieve data
@@ -12,3 +16,14 @@ class PostListView(ListView):
     #context object name allows us to change the variable name on how we call it inside of templates
     context_object_name = "posts"
 # Create your views here.
+
+class PostDetailView(DetailView): #Get Rquest -> single object/element
+    template_name = "posts/detail.html"
+    model = Post
+    context_object_name = "single_post"
+
+class PostCreateView(CreateView): #post request -> empty form(html)
+    template_name = "posts/delete.html"
+    model = Post
+    # fields attrib is a list that lets us enable/disable inputs to render in html
+    fields = ["title","subtitle", "body"]
